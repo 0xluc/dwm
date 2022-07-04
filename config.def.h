a803caa7115729e1894e9d7342e6158777e593d7
silverscalp <lucasbrodrigues14@gmail.com>
2022-07-04 13:18:47 -0300

    config

--- config.def.h
+++ config.def.h
@@ -35,7 +35,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Telegram",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "librewolf",  NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "Blueberry.py",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "blueberry",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "code-oss",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont};
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
