f376595b9bc7c5f8b2a20a82ca72ee0fb80f8de5
0xluc <lucasbrodrigues14@gmail.com>
2023-04-02 06:43:48 -0300

    changed font

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char statussep         = ';';      /* separator between status bars */
-static const char *fonts[]          = { "BlexMono Nerd Font:size=9:antialias=true:autohint=true" };
-static const char dmenufont[]       = "BlexMono Nerd Font:size=9:antialias=true:autohint=true";
+static const char *fonts[]          = { "Inconsolata:size=12:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Inconsolata:size=12:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
